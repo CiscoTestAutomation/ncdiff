2c12538ee41140997d3ca0204c4a3487363d8abb
yuekyang <yuekyang@cisco.com>
2025-04-11 13:48:16 -0400

    Improve pos in statement

--- src/ncdiff/__init__.py
+++ src/ncdiff/__init__.py
@@ -10,16 +10,18 @@ __copyright__ = 'Cisco Systems, Inc.'
 
 # __import__('pkg_resources').declare_namespace(__name__)
 
+import os
 import pprint
 from lxml import etree
 from ncclient import operations, transport
-from collections import OrderedDict
+from pyang import error, yang_parser, statements
 
 from .model import Model, ModelDownloader, ModelCompiler, ModelDiff
 from .config import Config, ConfigDelta
 from .manager import ModelDevice
 from .composer import Tag
 from .runningconfig import RunningConfigDiff
+
 
 def _repr_rpcreply(self):
     return '<{}.{} {} at {}>'.format(self.__class__.__module__,
@@ -164,3 +166,111 @@ def _wrap(self, subele):
 
 operations.edit.EditConfig.request = request
 operations.rpc.RPC._wrap = _wrap
+
+
+class Position(object):
+    __slots__ = (
+        'ref',
+        'line',
+        'top',
+        'uses_pos',
+        'first_line',
+        'last_line',
+    )
+
+    def __init__(self, ref):
+        self.ref = ref
+        self.line = 0
+        self.top = None
+        self.uses_pos = None
+        self.first_line = None
+        self.last_line = None
+
+    def __str__(self):
+        return self.label()
+
+    def label(self, basename=False):
+        ref = self.ref
+        if basename:
+            ref = os.path.basename(ref)
+        s = ref + ':' + str(self.line)
+        if self.uses_pos is None:
+            return s
+        else:
+            return str(self.uses_pos) + ' (at ' + s + ')'
+
+
+error.Position = Position
+
+
+def _parse_statement(self, parent):
+    first_line = (self.pos.line, self.tokenizer.offset)
+
+    # modification: when the --keep-comments flag is provided,
+    # we would like to see if a statement is a comment, and if so
+    # treat it differently than we treat keywords further down
+    if self.ctx.keep_comments:
+        cmt, is_line_end, is_multi_line = self.tokenizer.get_comment(
+            self.last_line)
+        if cmt is not None:
+            stmt = statements.new_statement(self.top,
+                                            parent,
+                                            self.pos,
+                                            '_comment',
+                                            cmt)
+            stmt.is_line_end = is_line_end
+            stmt.is_multi_line = is_multi_line
+            # just ignore Comments outside the module
+            if parent is not None:
+
+                stmt.pos.first_line = first_line
+                stmt.pos.last_line = (self.last_line, self.tokenizer.offset)
+
+                return stmt
+
+    keywd = self.tokenizer.get_keyword()
+    # check for argument
+    tok = self.tokenizer.peek()
+    if tok == '{' or tok == ';':
+        arg = None
+        argstrs = None
+    else:
+        argstrs = self.tokenizer.get_strings()
+        arg = ''.join([a[0] for a in argstrs])
+    # check for YANG 1.1
+    if keywd == 'yang-version' and arg == '1.1':
+        self.tokenizer.is_1_1 = True
+        self.tokenizer.strict_quoting = True
+
+    stmt = statements.new_statement(self.top, parent, self.pos, keywd, arg)
+
+    if self.ctx.keep_arg_substrings and argstrs is not None:
+        stmt.arg_substrings = argstrs
+    if self.top is None:
+        self.pos.top = stmt
+        self.top = stmt
+
+    # check for substatements
+    tok = self.tokenizer.peek()
+    if tok == '{':
+        self.tokenizer.skip_tok()  # skip the '{'
+        self.last_line = self.pos.line
+        while self.tokenizer.peek() != '}':
+            substmt = self._parse_statement(stmt)
+            stmt.substmts.append(substmt)
+        self.tokenizer.skip_tok()  # skip the '}'
+    elif tok == ';':
+        self.tokenizer.skip_tok()  # skip the ';'
+    else:
+        error.err_add(self.ctx.errors, self.pos, 'INCOMPLETE_STATEMENT',
+                      (keywd, tok))
+        raise error.Abort
+    self.last_line = self.pos.line
+
+    stmt.pos.first_line = first_line
+    stmt.pos.last_line = (self.last_line, self.tokenizer.offset)
+
+    return stmt
+
+
+yang_parser.YangParser._parse_statement = _parse_statement
